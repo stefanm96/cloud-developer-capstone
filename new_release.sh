3adc611024dc7c9089a623c78f018b8100f983ee
Stefan Michel <stefan.michel@lhind.dlh.de>
2020-09-30 14:10:03 +0200

    update release

--- new_release.sh
+++ new_release.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 # author: LHIND
 
-export VERSION=3.1.0
+export VERSION=3.3.0
 
 git checkout dev
 sed -i "s/version=.*/version="${VERSION}"/g" gradle.properties
 #./gradlew clean build
+git add gradle.properties
 git commit -m "update version to ${VERSION}"
-git merge master
 git checkout master
+git merge dev
 git tag ${VERSION}
 git push origin master
-git branch release/version
+git branch release/${VERSION}
